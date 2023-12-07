b3c528cd90b44a595e0c7cb0af484541caeea65c
Bao Lam <thienbao0712@gmail.com>
2023-12-07 01:33:21 -0800

    Finshed Challenge 3

--- index.js
+++ index.js
@@ -29,26 +29,33 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (const game of games){   
         // create a new div element, which will become the game card
-
+        let div = document.createElement('div')
 
         // add the class game-card to the list
-
+        div.classList.add('game-card')
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
+        div.innerHTML = `<p>Name: ${game.name}</p>`;
 
+        let img = document.createElement('img') 
+        img.className = 'game-img'
+        img.src = `${game.img}`
+
+        div.appendChild(img)
 
         // append the game to the games-container
-
+        gamesContainer.appendChild(div)
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
+addGamesToPage(GAMES_JSON)
 
 
 /*************************************************************************************
